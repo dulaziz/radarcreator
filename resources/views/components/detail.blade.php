80cbefe4c96c6f9f0c27d207d191cef66b2c7674
dulaziz <abdulaziz.radarbogor@gmail.com>
2023-02-01 17:18:51 +0700

    Tes Marge 2

--- resources/views/components/detail.blade.php
+++ resources/views/components/detail.blade.php
@@ -10,7 +10,41 @@
                 </a>
             </div>
             <div class="col-md-6">
-                
+                <div class="mb-3">
+                    <h6 class="">Production Team</h6>
+                    <div class="row mb-1">
+                        <div class="col-md-3">
+                            <p class="mb-0">Group</p>
+                        </div>
+                        <div class="col-md-9">
+                            <p class="mb-0">: Radar Depok</p>
+                        </div>
+                    </div>
+                    <div class="row mb-1">
+                        <div class="col-md-3">
+                            <p class="mb-0">Uploade Date</p>
+                        </div>
+                        <div class="col-md-9">
+                            <p class="mb-0">: 01 Jan 2045</p>
+                        </div>
+                    </div>
+                    <div class="row mb-1">
+                        <div class="col-md-3">
+                            <p class="mb-0">Producer</p>
+                        </div>
+                        <div class="col-md-9">
+                            <p class="mb-0">: John Doe</p>
+                        </div>
+                    </div>
+                    <div class="row mb-1">
+                        <div class="col-md-3">
+                            <p class="mb-0">Writer</p>
+                        </div>
+                        <div class="col-md-9">
+                            <p class="mb-0">: Bruno Mars</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         
