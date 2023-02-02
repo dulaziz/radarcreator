8ae149deb4c6f1c379459d1c6ceb48cee26260a8
dulaziz <abdulaziz.radarbogor@gmail.com>
2023-02-02 11:45:53 +0700

    Add Revenue Page

--- resources/views/components/revenue.blade.php
+++ resources/views/components/revenue.blade.php
@@ -1,5 +1,6 @@
 <div class="container-fluid pt-4 px-4">
     <div class="bg-secondary rounded p-4">
-        @include('components.calender')
+        Revenue
     </div>
 </div>
+@include('components.uploadedList')
