1f2c14ae62f7b5425e247e2cccc072c7c8b06ed6
elvanhaz <elvanhazi@gmai.com>
2023-02-21 20:58:54 +0700

    dialy update

---
+++ database/migrations/2023_02_21_084756_create_users.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->enum('group', ['Radar Bogor', 'Radar Sukabumi', 'Radar Depok', 'Radar Bandung']);
+            $table->string('email');
+            $table->string('password');
+            $table->enum('jabatan', ['1', '2', '3', '4', '5']);
+            $table->enum('role', ['user', 'produser', 'admin', 'adminSosmed', 'superAdmin']);
+            $table->string('gambar');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
